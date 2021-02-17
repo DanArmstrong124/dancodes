1c135b4c9d4a007975f9eb3fc6ff359c2fb8f573
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-02-17 18:03:29 +0000

    Potential fix

--- examples/onepage/assets/js/custom.js
+++ examples/onepage/assets/js/custom.js
@@ -1,12 +1,13 @@
-var primary = $( "#primarycolour" );
-var secondary = $( "#secondarycolour" );
-var tertiary = $( "#tertiarycolour" );
-var text = $( "#textcolour" );
-
-var p1 = $( "#primarybackground" );
-
-
 setInterval(function(){
+    var primary = $( "#primarycolour" );
+    var secondary = $( "#secondarycolour" );
+    var tertiary = $( "#tertiarycolour" );
+    var text = $( "#textcolour" );
+
+    var p1 = $( "#primarybackground" );
+
+    if (primary.val() !== ""){
         var primaryval = primary.val();
         p1.css( "background-color", primaryval );
+    }
 }, 10);
