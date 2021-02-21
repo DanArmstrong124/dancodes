7c1d29196cb03163c3a2cce20b28c3d2dd5cf656
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-02-21 19:05:40 +0000

    Adding features

--- tab/js/click.js
+++ tab/js/click.js
@@ -1,6 +1,12 @@
 
 var pintglass = $( "#pint" );
 
+var per20 = $("#20");
+var per40 = $("#40");
+var per60 = $("#60");
+var per80 = $("#80");
+var per100 = $("#100");
+
 var pinttotal = $( "#pinttotal" );
 var moneytotal = $( "#moneytotal" );
 
@@ -13,6 +19,18 @@ pintglass.click(function() {
   pintamount = pintamount + clickpower;
 });
 
+/* Updating text values */
 setInterval(function() {
     pinttotal.html("PINTS: " + pintamount.toFixed(2));
+}, 10);
+
+/* Updating colour values */
+setInterval(function() {
+    if (pintamount < 0.19) {
+        per20.css( "background-color", white );
+        per40.css( "background-color", white );
+        per60.css( "background-color", white );
+        per80.css( "background-color", white );
+        per100.css( "background-color", white );
+    }
 }, 10);
