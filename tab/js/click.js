cb27b77e042cdd25884b494ec046443681424e3b
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-02-21 18:56:02 +0000

    Adding JS

---
+++ tab/js/click.js
@@ -0,0 +1,17 @@
+var pintglass = $( "#pint" );
+
+var pinttotal = $( "#pinttotal" );
+var moneytotal = $( "#moneytotal" );
+
+var pintamount = 0;
+var moneyamount = 0;
+
+var clickpower = 0.05;
+
+pintglass.click(function() {
+  pintamount = pintamount + clickpower;
+});
+
+setInterval(function() {
+    pinttotal.html("PINTS: " + pintamount);
+}, 10);
