0ea9c5f1a5431c61cf75ae60df4b0c38a20c04b3
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-05-25 15:55:21 +0000

    Test

---
+++ tab/js/savebeer/script.js
@@ -0,0 +1,36 @@
+var fill = document.getElementById("pintdrink");
+var btn = document.getElementById("btnpress");
+
+var ready = true;
+//to set the timeouts
+
+var totalpints = 3;
+// 6,448,000,000 ( 6448000000 )
+
+var currentpints = 0;
+//will be linked to database
+
+var fillsofar = totalpints / currentpints;
+// 3 / 0 so far
+
+btn.onclick(function() {
+    if (ready === true) {
+        currentpints = currentpints + 1;
+        ready = false;
+        setTimeout(function() {
+            //15 min timeout to turn the button back on
+            ready = true;
+        }, 900000);
+        //add 1 to pints and set to false
+    }
+    else if (ready === false) {
+        alert("You can only have one pint every 15 mins.")
+        //announce they have hit limit for the day/week/hour
+    }
+});
+
+setInterval(function(){
+    fill.style.height = fillsofar + "%";
+}, 100);
+
+//Every 0.1s the pint will refresh to keep filling every persons clicks
