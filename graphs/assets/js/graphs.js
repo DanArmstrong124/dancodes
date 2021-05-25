f53eba8b26347609fed992251d87e6aaf5ee2357
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-05-25 22:41:53 +0000

    Test

--- graphs/assets/js/graphs.js
+++ graphs/assets/js/graphs.js
@@ -1,5 +1,18 @@
+var h1 = document.getElementById("high1");
+var l1 = document.getElementById("low1");
+var m1 = document.getElementById("med1");
+var d1 = document.getElementById("dif1");
+
 var coin = "XRP";
 var high1 = 0.967;
 var low1 = 0.65;
 var med1 = high1 + low1 / 2;
 var dif1 = high1 - low1;
+
+
+setTimeout(function() {
+    h1.innerHTML = high1;
+    l1.innerHTML = low1;
+    m1.innerHTML = med1;
+    d1.innerHTML = dif1;
+}, 50);
