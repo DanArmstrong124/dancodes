d42a57abbc09de127470405a02d297c5b6be9b33
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-05-25 22:52:27 +0000

    Test

--- graphs/assets/js/graphs.js
+++ graphs/assets/js/graphs.js
@@ -1,3 +1,8 @@
+var h0 = document.getElementById("high0");
+var l0 = document.getElementById("low0");
+var m0 = document.getElementById("med0");
+var d0 = document.getElementById("dif0");
+
 var h1 = document.getElementById("high1");
 var l1 = document.getElementById("low1");
 var m1 = document.getElementById("med1");
@@ -14,6 +19,11 @@ var m3 = document.getElementById("med3");
 var d3 = document.getElementById("dif3");
 
 var coin = "XRP";
+
+var high0 = 0.9567;
+var low0 = 0.8151;
+var med0 = high0 + low0 / 2;
+var dif0 = high0 - low0;
 
 var high1 = 0.967;
 var low1 = 0.65;
@@ -32,6 +42,11 @@ var dif3 = high3 - low3;
 
 
 setTimeout(function() {
+    h0.innerHTML = high0;
+    l0.innerHTML = low0;
+    m0.innerHTML = med0;
+    d0.innerHTML = dif0;
+
     h1.innerHTML = high1;
     l1.innerHTML = low1;
     m1.innerHTML = med1;
