1df3caf6fe52afa6dba58ffddfe611421cc9fe0b
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-06-03 10:57:11 +0000

    Test

--- tab/js/savebeer/script.js
+++ tab/js/savebeer/script.js
@@ -52,6 +52,7 @@ btn.addEventListener("click", function() {
         //fillsofar = storageCurrentPints / totalpints * 100;
         //storageCurrentPints = parseInt(localStorage.getItem('currentpints'));
         //currentpints = storageCurrentPints; //CurrentPints = StoragePints
+        currentpints = storageCurrentPints;
         localStorage.setItem('currentpints', currentpints + 1); //Storage pints = current pints + 1
         currentpints = currentpints + 1;
         //localStorage.setItem('currentpints', parseInt(storageCurrentPints + 1));
