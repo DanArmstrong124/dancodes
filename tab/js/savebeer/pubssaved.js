5b4c8a4a876d075190a123ef1327441dd9dd9845
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-06-04 13:30:22 +0000

    Test

--- tab/js/savebeer/pubssaved.js
+++ tab/js/savebeer/pubssaved.js
@@ -6,10 +6,11 @@ setInterval(function(){
         parseInt(localStorage.setItem('savedpubs', 0));
         location.reload();
     }
-    if (currentpints >= 124) {
+    if (currentpints >= 124) {        
         storageCurrentPints = parseInt(localStorage.setItem('currentpints', 0));
         currentpints = storageCurrentPints;
         parseInt(localStorage.setItem('savedpubs', currentpubssaved + 1));
         currentpubssaved = pubssaved
+        alert("Congratultions, you have now saved " + currentpubssaved + " pubs! - Please check the achievements for more!")
     }
 },50);
