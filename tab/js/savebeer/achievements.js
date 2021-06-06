c3113ffb03c3898f4bc040706081bede6c2694a5
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-06-06 20:26:54 +0000

    Testing new achievement code

--- tab/js/savebeer/achievements.js
+++ tab/js/savebeer/achievements.js
@@ -14,43 +14,43 @@ var ta = localStorage.getItem('timeatthebar');
 
 
 setInterval(function(){
-    if ((fp !== true) && (fp !== false)){
+    if ((fp !== "true") && (fp !== "false")){
         localStorage.setItem('firstpint', false);
         location.reload();
     }
-    if ((dp !== true) && (dp !== false)){
+    if ((dp !== "true") && (dp !== "false")){
         localStorage.setItem('doubleparked', false);
         location.reload();
     }
-    if ((oo !== true) && (oo !== false)){
+    if ((oo !== "true") && (oo !== "false")){
         localStorage.setItem('outout', false);
         location.reload();
     }
-    if ((wk !== true) && (wk !== false)){
+    if ((wk !== "true") && (wk !== "false")){
         localStorage.setItem('weekender', false);
         location.reload();
     }
-    if ((tl !== true) && (tl !== false)){
+    if ((tl !== "true") && (tl !== "false")){
         localStorage.setItem('thelocal', false);
         location.reload();
     }
-    if ((bs !== true) && (bs !== false)){
+    if ((bs !== "true") && (bs !== "false")){
         localStorage.setItem('bigspender', false);
         location.reload();
     }
-    if ((pa !== true) && (pa !== false)){
+    if ((pa !== "true") && (pa !== "false")){
         localStorage.setItem('patriot', false);
         location.reload();
     }
-    if ((il !== true) && (il !== false)){
+    if ((il !== "true") && (il !== "false")){
         localStorage.setItem('ironliver', false);
         location.reload();
     }
-    if ((hr !== true) && (hr !== false)){
+    if ((hr !== "true") && (hr !== "false")){
         localStorage.setItem('hero', false);
         location.reload();
     }
-    if ((ta !== true) && (ta !== false)){
+    if ((ta !== "true") && (ta !== "false")){
         localStorage.setItem('timeatthebar', false);
         location.reload();
     }
