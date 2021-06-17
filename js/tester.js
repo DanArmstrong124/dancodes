bd919a88f7fb0935c3f850c6ffff3d697008b84c
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-06-17 13:56:46 +0000

    Adding JS

--- js/tester.js
+++ js/tester.js
@@ -1,4 +1,9 @@
 var testpass = localStorage.getItem('testerpass');
+
+var unlocked = new function(){
+    localStorage.setItem('testerpass', "tabdev");
+};
+
 setInterval(function(){
     if (testpass !== "tabdev"){
         localStorage.setItem('testerpass', false);
