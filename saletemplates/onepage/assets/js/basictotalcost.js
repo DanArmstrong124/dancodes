48df5fb86d81460ba49f4c2d92e8dd922a615188
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-07-13 16:16:35 +0000

    Adding final steps to basic package beginnings

--- saletemplates/onepage/assets/js/basictotalcost.js
+++ saletemplates/onepage/assets/js/basictotalcost.js
@@ -3,6 +3,15 @@ var buysec4 = $('#buysec4');
 var sec4 = $('#sec4');
 
 var totalcost = 250;
+var sec4cost = 30;
+
+buysec4.click(function(){
+    totalcost = totalcost + sec4cost;
+    buysec4.addClass('hidden');
+    buysec4.removeClass('show');
+    sec4.addClass('show');
+    sec4.removeClass('hidden');
+});
 
 setInterval(function(){
     cost.text("Total Cost: £" + totalcost);
