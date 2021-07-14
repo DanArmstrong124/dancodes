a51a908c0afeebb64d7391fc0cf8a87fd32d7f47
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-07-14 14:37:18 +0000

    Test

--- saletemplates/onepage/assets/js/colourpick.js
+++ saletemplates/onepage/assets/js/colourpick.js
@@ -1,1 +1,22 @@
 var wheel = $('#colourpick');
+
+var bg = $('#bgcolour');
+var hd = $('#hdcolour');
+var fn = $('#fncolour');
+
+var body = $('#body');
+var s1 = $('#s1');
+var s2 = $('#s2');
+var s3 = $('#s3');
+
+setInterval(function(){
+    bg = bg.value;
+    hd = hd.value;
+    fn = fn.value;
+    if (bg !== "") {
+        body.style.background = bg;
+        s1.style.background = bg;
+        s2.style.background = bg;
+        s3.style.background = bg;
+    }
+}, 100);
