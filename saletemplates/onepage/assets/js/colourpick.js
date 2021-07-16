58d7cc2cd77640cc76be022ed996368d8138d90b
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-07-16 13:45:28 +0000

    Adding colours code

--- saletemplates/onepage/assets/js/colourpick.js
+++ saletemplates/onepage/assets/js/colourpick.js
@@ -9,11 +9,15 @@ var s1 = $('#s1');
 var s2 = $('#s2');
 var s3 = $('#s3');
 
+var nav = $('#nav-bar');
+
 setInterval(function(){
     //if (bg.value() !== "") {
         body.css( "background-color", document.getElementById('bgcolour').value);
-        s1.css( "background-color", $('#bgcolour').value);
-        s2.css( "background-color", $('#bgcolour').value);
-        s3.css( "background-color", $('#bgcolour').value);
+        s1.css( "background-color", document.getElementById('bgcolour').value);
+        s2.css( "background-color", document.getElementById('bgcolour').value);
+        s3.css( "background-color", document.getElementById('bgcolour').value);
+        nav.css( "background-color", document.getElementById('hdcolour').value);
+        body.css( "color", document.getElementById('fncolour').value);
     //}
 }, 100);
