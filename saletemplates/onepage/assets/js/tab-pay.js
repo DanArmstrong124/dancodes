bb2055e1077095a19506dfbfebc8f95e3055a0ef
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-07-20 15:51:17 +0000

    Test

--- saletemplates/onepage/assets/js/tab-pay.js
+++ saletemplates/onepage/assets/js/tab-pay.js
@@ -3,27 +3,18 @@ var mobile = document.getElementById('mobile');
 
 function responsiveDesign(x) {
     if (x.matches) { // If media query matches
-      document.body.style.backgroundColor = "yellow";
+        desktop.classList.add("hidden");
+        mobile.classList.add("shown");
+        desktop.classList.remove("shown");
+        mobile.classList.remove("hidden");
     } else {
-      document.body.style.backgroundColor = "pink";
+        desktop.classList.add("shown");
+        mobile.classList.add("hidden");
+        desktop.classList.remove("hidden");
+        mobile.classList.remove("shown");
     }
   }
 
 var x = window.matchMedia("(max-width: 700px)")
 responsiveDesign(x) // Call listener function at run time
 x.addListener(responsiveDesign) // Attach listener function on state changes
-
-setInterval(function() {
-    if ((window.screen.width > 828) && (window.screen.height > 1792)) {
-        desktop.classList.add("shown");
-        mobile.classList.add("hidden");
-        desktop.classList.remove("hidden");
-        mobile.classList.remove("shown");
-    }
-    else {
-        desktop.classList.add("hidden");
-        mobile.classList.add("shown");
-        desktop.classList.remove("shown");
-        mobile.classList.remove("hidden");
-    }
-}, 50);
