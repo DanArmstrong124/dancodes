996732a79e98ac47017c98684f3a5d46f57fe771
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-07-20 15:49:08 +0000

    Test

--- saletemplates/onepage/assets/js/tab-pay.js
+++ saletemplates/onepage/assets/js/tab-pay.js
@@ -1,6 +1,18 @@
 var desktop = document.getElementById('desktop');
 var mobile = document.getElementById('mobile');
 
+function responsiveDesign(x) {
+    if (x.matches) { // If media query matches
+      document.body.style.backgroundColor = "yellow";
+    } else {
+      document.body.style.backgroundColor = "pink";
+    }
+  }
+
+var x = window.matchMedia("(max-width: 700px)")
+responsiveDesign(x) // Call listener function at run time
+x.addListener(responsiveDesign) // Attach listener function on state changes
+
 setInterval(function() {
     if ((window.screen.width > 828) && (window.screen.height > 1792)) {
         desktop.classList.add("shown");
