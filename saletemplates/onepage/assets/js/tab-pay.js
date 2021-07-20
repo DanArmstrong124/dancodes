a1e6584e661612a4d033588b720a97851ab73967
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-07-20 15:44:45 +0000

    TEst

--- saletemplates/onepage/assets/js/tab-pay.js
+++ saletemplates/onepage/assets/js/tab-pay.js
@@ -1,2 +1,17 @@
 var desktop = document.getElementById('desktop');
 var mobile = document.getElementById('mobile');
+
+setInterval(function() {
+    if ((window.screen.width > 828) && (window.screen.height > 1792)) {
+        desktop.classList.add("shown");
+        mobile.classList.add("hidden");
+        desktop.classList.remove("hidden");
+        mobile.classList.remove("shown");
+    }
+    else {
+        desktop.classList.add("hidden");
+        mobile.classList.add("shown");
+        desktop.classList.remove("shown");
+        mobile.classList.remove("hidden");
+    }
+}, 50);
