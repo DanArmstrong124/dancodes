b625e9e8ee7b49e1abbc5fdd5df8aefa3d112a0a
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-11-18 14:34:50 +0000

    Added p

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ setInterval(function(){
 loginbtn.addEventListener("click", function(){
     logincreds = logincredsinput;
     localStorage.setItem('testerpass', logincreds);
-    if (logincreds === "tabdev2021"){
+    if (logincreds === "derek"){
         alert("Login successful, please re-input the url you are looking for!")
     }
     else{
